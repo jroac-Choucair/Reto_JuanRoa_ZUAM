bf9dfa2e0323b05f07a625fbcfcd9bccfdcd0820
jroac <jroac@choucairtesting.com>
2021-10-12 23:25:00 -0500

    Se crea tasks PrepararCompra

---
+++ src/main/java/com/choucair/moviles/app/tasks/PrepararCompra.java
@@ -0,0 +1,27 @@
+package com.choucair.moviles.app.tasks;
+
+import com.choucair.moviles.app.interactions.choucair.TakeScreenshot;
+import com.choucair.moviles.app.interactions.choucair.swaglabs.AgregarArticulo;
+import com.choucair.moviles.app.models.Usuario;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+
+import static com.choucair.moviles.app.ui.HomeUI.BTN_LISTAR;
+
+public class PrepararCompra implements Task {
+
+    Usuario usuario;
+    public PrepararCompra(Usuario usuario) {this.usuario = usuario;}
+    public static PrepararCompra exitosamente(Usuario usuario){ return Tasks.instrumented(PrepararCompra.class, usuario);}
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Click.on(BTN_LISTAR),
+                AgregarArticulo.visible(usuario.getProducto()),
+                TakeScreenshot.asEvidence()
+        );
+    }
+}
