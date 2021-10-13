b3f6f52888f1bf2f4302bcaccfcaebf943ce6b0c
jroac <jroac@choucairtesting.com>
2021-10-13 10:34:47 -0500

    Se refactorizan imports en la tasks Teminarcompra y en la Interfaz de usuario HomeUI

--- src/main/java/com/choucair/moviles/app/tasks/TerminaLaCompra.java
+++ src/main/java/com/choucair/moviles/app/tasks/TerminaLaCompra.java
@@ -1,19 +1,13 @@
 package com.choucair.moviles.app.tasks;
 
-import com.choucair.moviles.app.interactions.choucair.Espera;
-import com.choucair.moviles.app.interactions.choucair.TakeScreenshot;
 import com.choucair.moviles.app.interactions.choucair.swaglabs.CargarInformacionYCheckout;
 import com.choucair.moviles.app.interactions.choucair.swaglabs.IrAlCarritoYCheckout;
 import com.choucair.moviles.app.interactions.choucair.swaglabs.IrAlResumenYTerminar;
-import com.choucair.moviles.app.interactions.choucair.swaglabs.Scroll;
+
 import com.choucair.moviles.app.models.Usuario;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
-
-import static com.choucair.moviles.app.ui.CompraUI.*;
 
 public class TerminaLaCompra implements Task {
 
--- src/main/java/com/choucair/moviles/app/ui/HomeUI.java
+++ src/main/java/com/choucair/moviles/app/ui/HomeUI.java
@@ -4,5 +4,6 @@ import io.appium.java_client.MobileBy;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class HomeUI {
+
     public static final Target BTN_LISTAR = Target.the("Boton listar productos").located(MobileBy.AccessibilityId("test-Alternar"));
 }
