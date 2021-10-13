778cc0cf3bb2ee6be9b4ab059b7eb84a54b99248
jroac <jroac@choucairtesting.com>
2021-10-12 23:07:17 -0500

    Se crea modelo Usuario para almacenar  datos de la prueba y la tasks ReaizarLogin

---
+++ src/main/java/com/choucair/moviles/app/models/Usuario.java
@@ -0,0 +1,35 @@
+package com.choucair.moviles.app.models;
+
+public class Usuario {
+
+    String usuario;
+    String contrasena;
+    String producto;
+    String nombre;
+    String apellido;
+    String codpostal;
+
+    public String getUsuario() { return usuario; }
+
+    public void setUsuario(String usuario) { this.usuario = usuario; }
+
+    public String getContrasena() { return contrasena; }
+
+    public void setContrasena(String contrasena) { this.contrasena = contrasena; }
+
+    public String getProducto() { return producto; }
+
+    public void setProducto(String producto) { this.producto = producto; }
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellido() { return apellido; }
+
+    public void setApellido(String apellido) { this.apellido = apellido; }
+
+    public String getCodpostal() { return codpostal; }
+
+    public void setCodpostal(String codpostal) { this.codpostal = codpostal; }
+}
---
+++ src/main/java/com/choucair/moviles/app/tasks/RealizarLogin.java
@@ -0,0 +1,36 @@
+package com.choucair.moviles.app.tasks;
+
+import com.choucair.moviles.app.interactions.choucair.TakeScreenshot;
+import com.choucair.moviles.app.models.Usuario;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static com.choucair.moviles.app.ui.LoginUI.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isPresent;
+
+public class RealizarLogin implements Task{
+
+        Usuario usuario;
+        public RealizarLogin(Usuario usuario) {this.usuario = usuario;}
+        public static RealizarLogin exitoso(Usuario credenciales){return Tasks.instrumented(RealizarLogin.class,credenciales);}
+
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                WaitUntil.the(TXT_USUARIO, isPresent()).forNoMoreThan(5).seconds(),
+                Enter.theValue(usuario.getUsuario()).into(TXT_USUARIO),
+                TakeScreenshot.asEvidence(),
+                Enter.theValue(usuario.getContrasena()).into(TXT_CONTRASENA),
+                TakeScreenshot.asEvidence(),
+                Click.on(BTN_LOGIN),
+                TakeScreenshot.asEvidence()
+
+        );
+    }
+}
+
